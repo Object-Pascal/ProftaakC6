52d06cb92e944dd61284a7ccfb76694076b0a8ab
Stijn Verhelpen <sverhelp@avans.nl>
2018-09-13 14:14:20 +0200

    Tests toegevoegd

--- AWS/src/ValueConverterTests.java
+++ AWS/src/ValueConverterTests.java
@@ -1,6 +1,9 @@
 public class ValueConverterTests {
     public static void main(String[] args) {
         testTemperature();
+        testWindDirection();
+        testUvIndex();
+        testRainRate();
     }
 
     public static void testTemperature() {
@@ -15,5 +18,29 @@ public class ValueConverterTests {
         returnedData = ValueConverter.temperature(testValueB);
 
         System.out.println("Temperature outside = " + returnedData + " | Has to be: " + answerB);
+    }
+
+    public static void testWindDirection() {
+        short testValue = 277;
+        String answer = "277°";
+        String directionAnswer = ValueConverter.windDirection(testValue);
+
+        System.out.println("Direction Calculated: " + directionAnswer + " | Should be: " + answer );
+    }
+
+    public static void testUvIndex() {
+        short testValue = 38;
+        double answer = 3.8;
+        double calculated = ValueConverter.uvIndex(testValue);
+
+        System.out.println("UVIndex calculated = " + calculated + " | Should be: " + answer);
+    }
+
+    public static void testRainRate() {
+        short testValue = 185;
+        double answer = 46.99;
+        double calculated = ValueConverter.rainMeter(testValue);
+
+        System.out.println("RainRate calculated = " + calculated + " | Should be: " + answer);
     }
 }
