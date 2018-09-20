978d5b481d1466594a87c32764dc627f7333a760
Mathieu Blok <Mathieuga@live.nl>
2018-09-20 11:33:25 +0200

    Wind chill commit

--- AWS/src/Measurement.java
+++ AWS/src/Measurement.java
@@ -8,4 +8,30 @@ public class Measurement {
 
     public String getTimeStamp() { return timeStamp; }
     public void setTimeStamp(String value) { this.timeStamp = value; }
+
+    @Override
+    public String toString()
+    {
+        String s = "RawMeasurement:"
+                + "\nstationId = \t" + stationId
+                + "\ndateStamp = \t" + dateStamp
+                + "\nbarometer = \t" + barometer
+                + "\ninsideTemp = \t" + insideTemp
+                + "\ninsideHum = \t" + insideHum
+                + "\noutsideTemp = \t" + outsideTemp
+                + "\nwindSpeed = \t" + windSpeed
+                + "\navgWindSpeed = \t" + avgWindSpeed
+                + "\nwindDir = \t" + windDir
+                + "\noutsideHum = \t" + outsideHum
+                + "\nrainRate = \t" + rainRate
+                + "\nUVLevel = \t" + UVLevel
+                + "\nsolarRad = \t" + solarRad
+                + "\nxmitBatt = \t" + xmitBatt
+                + "\nbattLevel = \t" + battLevel
+                + "\nforeIcon = \t" + foreIcon
+                + "\nsunrise = \t" + sunrise
+                + "\nsunset = \t" + sunset;
+        return s;
+    }
+
 }
