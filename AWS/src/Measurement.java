0b324102845a10cc42cb8bf44d72151f4d2abeaa
hanno00 <brandwijkh@gmail.com>
2018-09-20 10:24:56 +0200

    Measurement set get push

--- AWS/src/Measurement.java
+++ AWS/src/Measurement.java
@@ -1,3 +1,11 @@
 public class Measurement {
-    
+    public Measurement() {
+        //RawMeasurement rawMeasurement = DatabaseConnection.getMostRecentMeasurement();
+        //System.out.println(rawMeasurement);
+    }
+
+    private String timeStamp;
+
+    public String getTimeStamp() { return timeStamp; }
+    public void setTimeStamp(String value) { this.timeStamp = value; }
 }
